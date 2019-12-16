00fa757773eeb6b30d9ae609ebead400131d1eb4
Morgan Roderick <morgan@roderick.dk>
2019-12-16 18:19:15 +0100

    Coverage: mouse support

--- test/mouse-support.test.js
+++ test/mouse-support.test.js
@@ -24,7 +24,7 @@ describe("Plete", function() {
         sinon.reset();
       });
 
-      context("when clicking on a suggestion", function() {
+      context("when left clicking on a suggestion", function() {
         it("calls the callback with the selected value", function() {
           const target = this.list.querySelector("plete-item:nth-child(2)");
 
@@ -62,6 +62,50 @@ describe("Plete", function() {
           const target = this.list.querySelector("plete-item:nth-child(2)");
 
           fireEvent.click(target, { button: 0 });
+
+          assert.equals(document.activeElement, this.input);
+        });
+      });
+
+      context("when right clicking on a suggestion", function() {
+        it("selects does nothing", function() {
+          const target = this.list.querySelector("plete-item:nth-child(2)");
+
+          const expectedValue = target.getAttribute("value");
+
+          refute.isUndefined(expectedValue);
+          refute.isNull(expectedValue);
+
+          fireEvent.click(target, { button: 2 });
+
+          refute.called(this.fakeSelect);
+        });
+
+        it("does not update the value of the input", function() {
+          const target = this.list.querySelector("plete-item:nth-child(2)");
+
+          const originalValue = this.input.value;
+
+          refute.isUndefined(originalValue);
+          refute.isNull(originalValue);
+
+          fireEvent.click(target, { button: 2 });
+
+          assert.equals(this.input.value, originalValue);
+        });
+
+        it("does not close the suggestions panel", function() {
+          const target = this.list.querySelector("plete-item:nth-child(2)");
+
+          fireEvent.click(target, { button: 2 });
+
+          refute.isNull(document.querySelector("plete-list"));
+        });
+
+        it("keeps focus on the input", function() {
+          const target = this.list.querySelector("plete-item:nth-child(2)");
+
+          fireEvent.click(target, { button: 2 });
 
           assert.equals(document.activeElement, this.input);
         });
